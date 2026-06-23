2b703f1d185e4df834e4a6976e0948209206dd52
andre luis <andrelsoaresf@gmail.com>
2026-06-22 22:40:53 -0300

    add função temVencedor

--- main.c
+++ main.c
@@ -1,9 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 //jogador 1 = 0, jogador 2 = 1
 //1 = o, 4 = x
 
-void desenharTabuleiro(int tabuleiro[3][3]){
+int temVencedor(int **tabuleiro){
+    int soma = 0;
+
+    int tabuleiro[3][3] = {{1,1,1},{0,0,0},{0,0,0}};
+
+    //soma linhas
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            soma += tabuleiro[i][j];
+        }
+        if (soma == 3)
+            return 1;
+        else if (soma == 12)
+            return 2;
+        else
+            soma = 0;
+        
+    }
+    //soma colunas
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            soma += tabuleiro[j][i];
+        }
+        if (soma == 3)
+            return 1;
+        else if (soma == 12)
+            return 2;
+        else
+            soma = 0;
+        
+    }
+
+    //soma diagonal principal
+    for (int i = 0; i < 3; i++){
+        for (int j; j < 3; j++){
+            if (i == j)
+                soma += tabuleiro[i][j];
+        }
+    }
+    if (soma == 3)
+            return 1;
+        else if (soma == 12)
+            return 2;
+        else
+            soma = 0;
+
+    //soma diagonal secundaria
+    for (int i = 0; i < 3; i++){
+        for (int j = 2; j >= 0; j--){
+            if (i + j == 2)
+                soma += tabuleiro[i][j];
+        }
+    }
+    if (soma == 3)
+            return 1;
+        else if (soma == 12)
+            return 2;
+        else
+            soma = 0;
+    
+    if (soma = 0)
+        return 0;
+}
+
+
+void desenharTabuleiro(int **tabuleiro){
+    printf("\033[2J\033[H");
+
     for (int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
             printf(" %d ", tabuleiro[i][j]);
@@ -15,8 +84,8 @@ void desenharTabuleiro(int tabuleiro[3][3]){
             printf("---------------\n");
     }
 }
-void marcarJogada(int x, int y, int jogador){
-    int tabuleiro[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
+
+void marcarJogada(int x, int y, int jogador, int **tabuleiro){
 
     if (jogador == 0){
         tabuleiro[x][y] = 1;
@@ -25,9 +94,20 @@ void marcarJogada(int x, int y, int jogador){
     }
 
     desenharTabuleiro(tabuleiro);
+    temVencedor(tabuleiro);
 }
 
 int main(){
+    int **tabuleiro = malloc(3 * sizeof(int));
+    for (int i = 0; i < 3; i++){
+        tabuleiro[i] = malloc(3 * sizeof(int));
+    }
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            tabuleiro[i][j] = 0;
+        }
+    }
+
     int x, y, jogador;
 
     printf("quem vai ser o primeiro a jogar? (0 ou 1): ");
@@ -35,6 +115,11 @@ int main(){
     printf("digite qual posição do tabuleiro pra colocar (x,y): ");
     scanf("%d, %d", &x ,&y);
 
-    marcarJogada(x, y, jogador);
+    marcarJogada(x, y, jogador, tabuleiro);
+
+
+
+    free(tabuleiro);
+    **tabuleiro = NULL;
     return 0;
 }
